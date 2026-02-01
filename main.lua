a75f177571f995e5a0ff1c5d13e975e04bc0f7d9
amabar49-boop <amabar49@gmail.com>
2026-02-01 13:26:29 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+local Library = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"
+))()
+
+getgenv().Library = Library
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/amabar49-boop/silver-rly/main/features.lua"
+))()
