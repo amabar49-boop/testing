1b482efb34d1dd72d1ac14ea2abef8119e1eda44
amabar49-boop <amabar49@gmail.com>
2026-02-02 13:05:50 +0700

    Update features.lua

--- features.lua
+++ features.lua
@@ -1,22 +1,17 @@
 -- features.lua
-local Vars = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/amabar49-boop/testing/main/ui.lua"
-))()
-
 local M = {}
 
-function M.Start()
+function M.Start(Vars)
 	task.spawn(function()
 		while task.wait(Vars.Delay) do
 			if Vars.AutoTap then
-				-- GANTI remote tap
-				-- contoh:
-				-- game.ReplicatedStorage.Events.Tap:FireServer()
+				print("Auto Tap Jalan")
+				-- isi remote tap di sini
 			end
 
 			if Vars.AutoRebirth then
-				-- GANTI remote rebirth
-				-- game.ReplicatedStorage.Events.Rebirth:FireServer()
+				print("Auto Rebirth Jalan")
+				-- isi remote rebirth di sini
 			end
 		end
 	end)
